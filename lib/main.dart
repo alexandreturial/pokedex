54bc92370d9224ae5e66ae2ad811d7b4f5ac330b
xande <aleturial8@gmail.com>
2021-04-02 20:12:19 -0300

    info screem

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:poke_project/Components/Card.dart';
 
@@ -7,18 +6,45 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
-    
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Container(color: Colors.purple,)
+      home: Container(
+        color: Colors.purple,
+        child: Stack(
+          children: [
+            Column(children: [
+              Expanded(child: Container(
+                padding: EdgeInsets.all(50),
+                child: Text(
+                  'Gengar'
+                ),
+              )),
+              Expanded(
+                child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(25),
+                        topLeft: Radius.circular(25))),
+              ))
+            ]),
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 100),
+              child: Image.network(
+                'https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png',
+                height: 250,
+                fit: BoxFit.cover,
+              ))
+          ],
+        ),
+      ),
     );
   }
 }
 
-
+//
 // 
